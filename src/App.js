f409cecd87d7b6e866c81b549f6a1f8d6b12238b
kanchankk11 <nagkanchan5@gmail.com>
2023-06-28 23:40:09 +0530

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={LoginPage()}></Route>
+            <Route path="/home" element={HomePage()}></Route>
+            <Route path="/profile/:userId" element={ProfilePage()}></Route>
+          </Routes>
+        </ThemeProvider>
+
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/scenes/navbar/index.jsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+    return <div>Home Page</div>
+    
+  }
+  
+  export default NavBar;
+  
